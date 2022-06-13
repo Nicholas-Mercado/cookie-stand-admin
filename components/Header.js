d93b241528397bfd0452069da499f43aeead5f05
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-06-12 19:29:14 -0700

    added user name on login

--- components/Header.js
+++ components/Header.js
@@ -3,12 +3,14 @@ import { useAuth } from "../contexts/auth";
 
 export default function Header(props) {
 const { user, logout } = useAuth();
+console.log(user)
 if (props.user) {
 return (
         
       <header className="relative flex justify-between py-6 text-4xl font-semibold text-gray-800 prex-8 bg-emerald-500">
         <h1>Cookie Stand Admin</h1>
         <div className=''>
+        <div className='float-left px-4 py-1 mx-2 my-5 font-medium rounded-md bg-emerald-50 text-zinc-600'>{user.username}</div>
         <button
             className='float-left px-4 py-1 mx-2 my-5 font-medium rounded-md bg-emerald-50 text-zinc-600'
             onClick={logout}
